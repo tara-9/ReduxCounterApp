82e24372c8c35371ab64e7619a425f5573179d02
tara97 <taraprasad9090@gmail.com>
2023-04-17 21:46:15 +0530

    Adding toogle handler

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -2,6 +2,7 @@ import classes from './Counter.module.css';
 import { useSelector, useDispatch } from 'react-redux';
 const Counter = () => {
   const counter = useSelector(store => store.counter)
+  const show = useSelector(store => store.showCounter)
   const disPatch = useDispatch()
   
   const incrementButtonHandler = () => {
@@ -11,14 +12,21 @@ const Counter = () => {
   const decrementButtonHandler = () => {
     disPatch({type:'decrement'})
   }
-  const toggleCounterHandler = () => {};
+
+  const increaseHandler = () => {
+    disPatch({type:'increase', amount:5})
+  }
+  const toggleCounterHandler = () => {
+    disPatch({type:'toggle'})
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      {show && <div className={classes.value}>{counter}</div>}
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
       <button onClick={incrementButtonHandler}>Increment</button>
+      <button onClick={increaseHandler}>Increse By 5</button>
       <button onClick={decrementButtonHandler}>Decrement</button>
     </main>
   );
--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,11 @@
 import {configureStore} from '@reduxjs/toolkit'
 
-const initialState = { counter: 0 }
+const initialState = { counter: 0, showCounter:true}
+
+console.log(initialState)
 
 function counterReducer(state = initialState, action) {
+  console.log("Inside Counter Reducer Method")
   // Check to see if the reducer cares about this action
   if (action.type === 'increment') {
     // If so, make a copy of `state`
@@ -21,9 +24,27 @@ function counterReducer(state = initialState, action) {
       counter: state.counter - 1
     }
   }
+
+  if(action.type === 'increase') {
+    return{
+      ...state,
+      counter: state.counter + action.amount
+    }
+  }
+
+  if(action.type === 'toggle') {
+    return{
+      ...state,
+      showCounter: !state.showCounter
+    }
+  }
   // otherwise return the existing state unchanged
   return state
+
+  
 }
+
+
 
 const store = configureStore({reducer: counterReducer})
 
